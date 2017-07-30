db1e041aa9ea44709ed14fa8501159d5894533e5
Graeme Lockley <graemel@no9.co.za>
2017-07-30 08:22:50 +0200

    chore: add reference to generative

--- test/Libs.js
+++ test/Libs.js
@@ -1,4 +1,5 @@
 module.exports = {
     Assertion: mrequire("core:Test.Unit.Assertion:1.1.0"),
+    Generative: mrequire("core:Test.Generative:0.0.1"),
     Unit: mrequire("core:Test.Unit:0.0.1")
 };
