df90c0e9e815c2b382f6168457ba171308e868fa
Graeme Lockley <graemel@no9.co.za>
2017-07-30 08:41:57 +0200

    test: add generative tests and test for tail

--- test/InfiniteStreamTest.js
+++ test/InfiniteStreamTest.js
@@ -1,4 +1,5 @@
 const Assertion = require("./Libs").Assertion;
+const Generative = require("./Libs").Generative;
 const Unit = require("./Libs").Unit;
 
 const Stream = require("../index.js");
@@ -7,8 +8,16 @@ const Stream = require("../index.js");
 const countStream = n =>
     Stream.Cons(n)(() => countStream(n + 1));
 
+const INTEGERS =
+    Generative.randoms().then(things => things.map(s => s.asIntInRange(-10000)(10000)));
+
 
 module.exports = Unit.Suite("Data.Collection.InfiniteStream")([
-    Unit.Test("head returns the first elements")(Assertion
-        .equals(1)(countStream(1).head()))
+    Unit.Test("head returns the first element")(
+        Generative.forAll(INTEGERS)(n => Assertion
+            .equals(n)(countStream(n).head()))),
+
+    Unit.Test("tail returns the rest of the elements")(
+        Generative.forAll(INTEGERS)(n => Assertion
+            .equals(n + 1)(countStream(n).tail().head())))
 ]);
